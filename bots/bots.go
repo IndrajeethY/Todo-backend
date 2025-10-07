aa17edf81803ef26632e8ed84197adb02306071a
IndrajeethY <indrajeeth0703@gmail.com>
2025-10-07 06:43:11 +0000

    formatted

--- bots/bots.go
+++ bots/bots.go
@@ -81,52 +81,52 @@ func (b *Bots) schedulerLoop() {
 }
 
 func (b *Bots) processReminders() {
-    now := time.Now().UTC()
-    var todos []models.Todo
-    if err := b.DB.Where("notify_enabled = ? AND completed = ?", true, false).Find(&todos).Error; err != nil {
-        return
-    }
-    for _, t := range todos {
-        if t.NextNotifyAt == nil {
-            now := time.Now().UTC()
-            t.NextNotifyAt = &now
-            _ = b.DB.Save(&t)
-        }
-        if t.DueDate != nil && now.After(*t.DueDate) {
-            continue
-        }
-        if t.NextNotifyAt.After(now) {
-            continue
-        }
-        msg := buildMessage(&t)
-        if t.TelegramEnabled && b.TgBot != nil && b.OwnerTelegram != "" {
-            chatID, err := strconv.ParseInt(b.OwnerTelegram, 10, 64)
-            if err == nil {
-                _, _ = b.TgBot.SendMessage(chatID, msg, &gotgbot.SendMessageOpts{ParseMode: "Markdown"})
-            } else {
-                log.Println("invalid OWNER_TG_ID:", err)
-            }
-        }
-        if t.DiscordEnabled && b.Discord != nil && b.OwnerDiscord != "" {
-            channel, err := b.Discord.UserChannelCreate(b.OwnerDiscord)
-            if err == nil {
-                _, _ = b.Discord.ChannelMessageSend(channel.ID, msg)
-            } else {
-                log.Println("failed to create discord dm channel:", err)
-            }
-        }
-        if t.NotifyFrequency > 0 && t.NextNotifyAt != nil {
-            next := t.NextNotifyAt.Add(time.Duration(t.NotifyFrequency) * time.Minute)
-            if t.DueDate != nil && next.After(*t.DueDate) {
-                t.NextNotifyAt = nil
-            } else {
-                t.NextNotifyAt = &next
-            }
-        } else {
-            t.NextNotifyAt = nil
-        }
-        _ = b.DB.Save(&t)
-    }
+	now := time.Now().UTC()
+	var todos []models.Todo
+	if err := b.DB.Where("notify_enabled = ? AND completed = ?", true, false).Find(&todos).Error; err != nil {
+		return
+	}
+	for _, t := range todos {
+		if t.NextNotifyAt == nil {
+			now := time.Now().UTC()
+			t.NextNotifyAt = &now
+			_ = b.DB.Save(&t)
+		}
+		if t.DueDate != nil && now.After(*t.DueDate) {
+			continue
+		}
+		if t.NextNotifyAt.After(now) {
+			continue
+		}
+		msg := buildMessage(&t)
+		if t.TelegramEnabled && b.TgBot != nil && b.OwnerTelegram != "" {
+			chatID, err := strconv.ParseInt(b.OwnerTelegram, 10, 64)
+			if err == nil {
+				_, _ = b.TgBot.SendMessage(chatID, msg, &gotgbot.SendMessageOpts{ParseMode: "Markdown"})
+			} else {
+				log.Println("invalid OWNER_TG_ID:", err)
+			}
+		}
+		if t.DiscordEnabled && b.Discord != nil && b.OwnerDiscord != "" {
+			channel, err := b.Discord.UserChannelCreate(b.OwnerDiscord)
+			if err == nil {
+				_, _ = b.Discord.ChannelMessageSend(channel.ID, msg)
+			} else {
+				log.Println("failed to create discord dm channel:", err)
+			}
+		}
+		if t.NotifyFrequency > 0 && t.NextNotifyAt != nil {
+			next := t.NextNotifyAt.Add(time.Duration(t.NotifyFrequency) * time.Minute)
+			if t.DueDate != nil && next.After(*t.DueDate) {
+				t.NextNotifyAt = nil
+			} else {
+				t.NextNotifyAt = &next
+			}
+		} else {
+			t.NextNotifyAt = nil
+		}
+		_ = b.DB.Save(&t)
+	}
 }
 
 func buildMessage(t *models.Todo) string {
